d16a129a6dd741fe63c7aaba92d87ba3c9beec6e
dev-fa <franco.a.dev@proton.me>
2023-02-25 14:03:25 +0800

    Rewrite project boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,9 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/js/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  devtool: 'inline-source-map',
 };
